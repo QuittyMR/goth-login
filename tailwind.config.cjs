8a5799f5ff25efe9b5ac7b055b45e6af52e3d672
quitty <dev@quitty.tech>
2022-05-21 13:03:17 +0200

    Spinner

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,6 @@ module.exports = {
     content: [
         './index.html',
         './src/**/*.{js,ts,html,svelte}',
-        './src/**/*.{js,ts,html,svelte}',
     ],
     theme: {
         extend: {
